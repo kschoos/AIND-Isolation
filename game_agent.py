e6cb5e20dc9c253bba7eea5894c54c68d5ce7b28
kschoos <kschoos@student.tugraz.at>
2017-07-23 19:04:57 +0200

    First submit

--- game_agent.py
+++ game_agent.py
@@ -10,88 +10,6 @@ class SearchTimeout(Exception):
     """Subclass base exception for code clarity. """
     pass
 
-def custom_score_generator_with_center(A, B, C, D):
-    def generated_custom_score(game, player):
-        """Calculate the heuristic value of a game state from the point of view
-        of the given player.
-
-        This should be the best heuristic function for your project submission.
-
-        Note: this function should be called from within a Player instance as
-        `self.score()` -- you should not need to call this function directly.
-
-        Parameters
-        ----------
-        game : `isolation.Board`
-            An instance of `isolation.Board` encoding the current state of the
-            game (e.g., player locations and blocked cells).
-
-        player : object
-            A player instance in the current game (i.e., an object corresponding to
-            one of the player objects `game.__player_1__` or `game.__player_2__`.)
-
-        Returns
-        -------
-        float
-            The heuristic value of the current game state to the specified player.
-        """
-        score = 0
-
-        opponent = game.get_opponent(player)
-
-        our_moves = float(len(game.get_legal_moves(player)))
-        their_moves = float(len(game.get_legal_moves(opponent)))
-
-        our_position = game.get_player_location(player)
-        our_distance_to_center = math.sqrt((game.width - our_position[0]) ** 2 + (game.height - our_position[1]) ** 2)
-
-        their_position = game.get_player_location(game.get_opponent(player))
-        their_distance_to_center = math.sqrt((game.width - their_position[0]) ** 2 + (game.height - their_position[1]) ** 2)
-
-        score += our_moves * A - their_moves * B + our_distance_to_center * C - their_distance_to_center * D
-
-        return score
-
-    return generated_custom_score
-
-def custom_score_generator(A, B):
-    def generated_custom_score(game, player):
-        """Calculate the heuristic value of a game state from the point of view
-        of the given player.
-
-        This should be the best heuristic function for your project submission.
-
-        Note: this function should be called from within a Player instance as
-        `self.score()` -- you should not need to call this function directly.
-
-        Parameters
-        ----------
-        game : `isolation.Board`
-            An instance of `isolation.Board` encoding the current state of the
-            game (e.g., player locations and blocked cells).
-
-        player : object
-            A player instance in the current game (i.e., an object corresponding to
-            one of the player objects `game.__player_1__` or `game.__player_2__`.)
-
-        Returns
-        -------
-        float
-            The heuristic value of the current game state to the specified player.
-        """
-        score = 0
-
-        opponent = game.get_opponent(player)
-
-        our_moves = float(len(game.get_legal_moves(player)))
-        their_moves = float(len(game.get_legal_moves(opponent)))
-
-        score += our_moves * A - their_moves * B
-
-        return score
-
-    return generated_custom_score
-
 
 def custom_score(game, player):
     """Calculate the heuristic value of a game state from the point of view
@@ -151,10 +69,6 @@ def custom_score_2(game, player):
     float
         The heuristic value of the current game state to the specified player.
     """
-
-    our_moves = float(len(game.get_legal_moves(player)))
-    their_moves = float(len(game.get_legal_moves(game.get_opponent(player))))
-
     our_position = game.get_player_location(player)
     our_distance_to_center = math.sqrt((game.width - our_position[0]) ** 2 + (game.height - our_position[1]) ** 2)
 
@@ -187,7 +101,6 @@ def custom_score_3(game, player):
     float
         The heuristic value of the current game state to the specified player.
     """
-
     return float(len(game.get_legal_moves(player))) - float(len(game.get_legal_moves(game.get_opponent(player))))
 
 
@@ -272,6 +185,26 @@ class MinimaxPlayer(IsolationPlayer):
         return best_move
 
     def minimize(self, game, max_depth, current_depth):
+        """ Find the move that minimizes the chances of the player 'self' winning on the given game board.
+        If the time is up, raises an exception and stops the current search.
+
+        Parameters
+        ----------
+        game : `isolation.Board`
+            An instance of `isolation.Board` encoding the current state of the
+            game (e.g., player locations and blocked cells).
+
+        max_depth: `int`
+            The maximum depth to search to.
+
+        current_depth: `int`
+            The current depth.
+
+        Returns
+        -------
+        float
+            The expected score of the move the move that minimizes the chances of the player `self` winning.
+        """
         if self.time_left() < self.TIMER_THRESHOLD:
             raise SearchTimeout()
 
@@ -287,6 +220,26 @@ class MinimaxPlayer(IsolationPlayer):
             for move in game.get_legal_moves()])
 
     def maximize(self, game, max_depth, current_depth):
+        """ Find the move that maximizes the chances of the player 'self' winning on the given game board.
+        If the time is up, raises an exception and stops the current search.
+
+        Parameters
+        ----------
+        game : `isolation.Board`
+            An instance of `isolation.Board` encoding the current state of the
+            game (e.g., player locations and blocked cells).
+
+        max_depth: `int`
+            The maximum depth to search to.
+
+        current_depth: `int`
+            The current depth.
+
+        Returns
+        -------
+        float
+            The expected score of the move the move that maximizes the chances of the player `self` winning.
+        """
         if self.time_left() < self.TIMER_THRESHOLD:
             raise SearchTimeout()
 
@@ -300,8 +253,6 @@ class MinimaxPlayer(IsolationPlayer):
         return max([
             self.minimize(game.forecast_move(move), max_depth, current_depth + 1)
             for move in game.get_legal_moves()])
-
-
 
     def minimax(self, game, depth):
         """Implement depth-limited minimax search algorithm as described in
